9eb02c0910110c57a32de834a89efd4e1204716c
Facundo-byte <facundodavidiascas@gmail.com>
2025-08-23 13:02:25 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,201 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function Interfaz() {
+  return (
+    <div className="mainContainer">
+      <div className="contIzquierdo">
+        <Information title="General information" />
+        <Information title="Professional summary" />
+        <Information title="Skills" />
+        <Information title="Education" />
+        <Information title="Work experience" />
+        <Information title="Custom information" />
+      </div>
+      <div className="contDerecho">
+        <Curriculum />
+      </div>
+    </div>
+  );
+}
+
+/*botones*/
+export function Information({ title }) {
+  const [expand, setExpand] = useState(false);
+
+  function handleClick() {
+    setExpand(!expand);
+  }
+
+  return (
+    <div className="Info">
+      <div className="Base">
+        <h2>{title}</h2>
+        <button onClick={handleClick}>X</button>
+      </div>
+      {expand ? <MoreInfo title={title} /> : null}
+    </div>
+  );
+}
+
+export function MoreInfo({ title }) {
+  switch (title) {
+    case "General information":
+      return (
+        <form className="Moreinfo">
+          <label>
+            Full name
+            <input type="text" />
+          </label>
+
+          <label>
+            Email
+            <input type="text" />
+          </label>
+
+          <label>
+            Phone number
+            <input type="text" />
+          </label>
+
+          <label>
+            Location
+            <input type="text" />
+          </label>
+
+          <button>Submit</button>
+        </form>
+      );
+    case "Professional summary":
+      return (
+        <form className="MoreInfo">
+          <label>
+            Professional summary
+            <input type="textarea" />
+          </label>
+
+          <button>Submit</button>
+        </form>
+      );
+    default:
+      return null;
+  }
+}
+
+/* cv */
+
+export function Curriculum() {
+  return (
+    <div className="mainCV">
+      <div className="General">
+        <h1>Ken Kaneki</h1>
+        <div className="Social">
+          <h3>kenkaneki@anteiku.com</h3>
+          <h3>7012341234</h3>
+          <h3>Tokyo, Japan</h3>
+        </div>
+      </div>
+
+      <div className="Summary">
+        <h2 className="Title">Professional Summary</h2>
+        <p>
+          Results-driven Software Engineer with 5 years of experience designing,
+          developing, and optimizing scalable applications and systems. Skilled
+          in full-stack development, cloud technologies, and agile
+          methodologies, with a strong focus on writing clean, maintainable code
+          and delivering high-quality solutions that meet business needs. Adept
+          at collaborating across cross-functional teams, solving complex
+          technical challenges, and continuously learning new tools and
+          frameworks to drive innovation and efficiency.
+        </p>
+      </div>
+
+      <div className="Skills">
+        <h2 className="Title">Skills</h2>
+        <ul className="ItemsSkills">
+          <li>JavaScript</li>
+          <li>JavaScript</li>
+          <li>JavaScript</li>
+          <li>JavaScript</li>
+          <li>JavaScript</li>
+          <li>JavaScript</li>
+          <li>JavaScript</li>
+          <li>JavaScript</li>
+          <li>JavaScript</li>
+          <li>JavaScript</li>
+          <li>JavaScript</li>
+          <li>JavaScript</li>
+          <li>JavaScript</li>
+        </ul>
+      </div>
+
+      <div className="Education">
+        <h2 className="Title">Education</h2>
+        <ul>
+          <li>
+            <EdandWork
+              place="Massachusetts Institute of Technology"
+              location="Massachusetts, United States"
+              startdate="20/08/22"
+              enddate="30/01/27"
+              title="Bachelor's in Computer Science"
+              desc="As a computer science major, I've learned how to think critically and solve problems using computational approaches. I've studied programming languages, algorithms, and data structures to build efficient software, while also gaining an understanding of computer systems, networks, and operating systems. I've explored areas like databases, cybersecurity, and artificial intelligence, which showed me how computer science connects to real-world applications. Through these experiences, I've developed strong problem-solving, logical reasoning, and analytical skills."
+            />
+          </li>
+        </ul>
+      </div>
+
+      <div className="Work">
+        <h2 className="Title">Work experience</h2>
+        <ul>
+          <li>
+            <EdandWork
+              place="Microsoft"
+              location="Seattle, United States"
+              startdate="31/01/07"
+              enddate="20/11/23"
+              title="Software Engineer"
+              desc="Developed and mantained software using mainly web technologies like JavaScript and TypeScript."
+            />
+          </li>
+
+          <li>
+            <EdandWork
+              place="Boston Dynamics"
+              location="Boston, United States"
+              startdate="30/11/24"
+              enddate="08/08/25"
+              title="Low Level Engineer"
+              desc="Developed drivers using mainly C and Assembly."
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export function EdandWork({
+  place,
+  location,
+  startdate,
+  enddate,
+  title,
+  desc,
+}) {
+  return (
+    <div className="Education">
+      <div className="LocationandDate">
+        <h3>
+          {place} | {location}
+        </h3>
+        <h3>
+          {startdate} - {enddate}
+        </h3>
+      </div>
+
+      <h3>{title}</h3>
+      <p>{desc}</p>
+    </div>
+  );
+}
